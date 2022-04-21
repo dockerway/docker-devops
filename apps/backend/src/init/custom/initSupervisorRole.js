324a9e886ae78daf6f8fa875b8e1d376d98cc813
Carolina Romero <romerocaroe@gmail.com>
2022-04-21 12:55:57 -0300

    Readonly Roles & Dracul Dependencies Updated

---
+++ apps/backend/src/init/custom/initSupervisorRole.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "supervisor",
+    permissions: [
+      "SECURITY_USER_CREATE",
+      "SECURITY_USER_DELETE",
+      "SECURITY_USER_EDIT",
+      "SECURITY_USER_SHOW",
+      "SECURITY_ROLE_SHOW_CHILD",
+      "SECURITY_GROUP_SHOW",
+      "SECURITY_DASHBOARD_SHOW",
+      "SECURITY_ADMIN_MENU",
+    ],
+    readonly: true
+}
