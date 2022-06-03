1f453f9ccdb52a72209cebe215669e8dba1d46fa
ramiro.sandoval <ramiro.sandoval@sondeos.com.ar>
2022-06-03 11:03:38 -0300

    Permissions translation added

--- apps/frontend/src/modules/devops/i18n/index.js
+++ apps/frontend/src/modules/devops/i18n/index.js
@@ -1,16 +1,18 @@
-import merge from 'deepmerge'
-import PlatformMessages from './messages/PlatformMessages'
-import ServiceMessages from './messages/ServiceMessages'
-import StackMessages from './messages/StackMessages'
-import EnvironmentMessages from './messages/EnvironmentMessages'
-import EnvironmentServiceMessages from './messages/EnvironmentServiceMessages'
+import merge from 'deepmerge';
+import PlatformMessages from './messages/PlatformMessages';
+import ServiceMessages from './messages/ServiceMessages';
+import StackMessages from './messages/StackMessages';
+import EnvironmentMessages from './messages/EnvironmentMessages';
+import EnvironmentServiceMessages from './messages/EnvironmentServiceMessages';
+import PermissionMessages from './messages/PermissionsMessages';
 
 const messages = merge.all([
     PlatformMessages,
     ServiceMessages,
     StackMessages,
     EnvironmentMessages,
-    EnvironmentServiceMessages
+    EnvironmentServiceMessages,
+    PermissionMessages
 ])
 
 export default messages;
