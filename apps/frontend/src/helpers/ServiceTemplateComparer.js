4e84cb994eebd1c153b988feb9bb335afe958021
ramiro.sandoval <ramiro_sando@outlook.com>
2023-05-11 17:02:37 -0300

    advances in ticket #80349 logic

--- apps/frontend/src/helpers/ServiceTemplateComparer.js
+++ apps/frontend/src/helpers/ServiceTemplateComparer.js
@@ -1,133 +1,164 @@
 /**
- * Creates a new instance of the `ServiceTemplateComparer` class.
- * @param {Object} template - The template to compare.
- * @param {Object} service - The service to compare.
- * @throws {Error} If the `template` or `service` parameters are not provided.
+ * Compares a service object against a service template object, and returns whether the service object
+ * matches the service template object in the relevant fields.
 */
 export default class ServiceTemplateComparer {
 
-    constructor(template, service) {
-
-        if (!template || !service) throw new Error('The class must be instantiated with a template and a service')
-
-        this.template = template
-        this.service = service
-    }
-
     /**
-     * An array of fields to compare between the template and the service.
-     * @type {Array<string>}
+     * @param {object} template - The service template object to compare the service object against.
+     * @param {object} service - The service object to compare against the service template object.
+     * @throws {Error} Will throw an error if the class is not instantiated with both a template and a service.
+    */
+    constructor(template, service) {
+      if (!template || !service) throw new Error('The class must be instantiated with a template and a service')
+  
+      this.template = template
+      this.service = service
+    }
+  
+    /**
+     * An array of the names of the fields to compare between the service and template objects.
     */
     fieldsToCompare = ['volumes', 'files', 'ports', 'envs', 'constraints']
+  
 
     /**
-     * Converts the `template` object into a `templateToServiceFormat` object that has the `service` object format and only has the propertie values listed in `fieldsToCompare`.
-     * @type {Object}
+     * A getter that returns the service template object in a format that matches the format of the service object
+     * (i.e., with irrelevant fields removed and remaining fields formatted to match the service object).
+     * @returns {object} An object containing only the relevant fields from the service template object in a format
+     * that matches the format of the service object.
     */
+
     get templateToServiceFormat() {
-        const templateToServiceFormat = {
-            volumes: [],
-            files: [],
-            ports: [],
-            envs: [],
-            constraints: []
-        }
-
-        this.template.volumes.forEach(templateVolume => {
-            templateToServiceFormat.volumes.push({ containerVolume: templateVolume })
-        })
-
-        this.template.files.forEach(templateFile => {
-            delete templateFile.hostPath
-            delete templateFile.fileContent
-            templateToServiceFormat.files.push({ ...templateFile })
-        })
-
-        this.template.ports.forEach(templatePort => {
-            templateToServiceFormat.ports.push({ containerPort: templatePort })
-        })
-
-        this.template.envs.forEach(templateEnv => {
-            delete templateEnv.defaultValue
-            templateToServiceFormat.envs.push(templateEnv)
-        })
-
-        this.template.constraints.forEach(templateConstraint => {
-            delete templateConstraint.defaultValue
-            templateToServiceFormat.constraints.push(templateConstraint)
-        })
-
-        return templateToServiceFormat
+      const templateToServiceFormat = { volumes: [], files: [], ports: [], envs: [], constraints: []}
+  
+      this.template.volumes.forEach(templateVolume => {
+        templateToServiceFormat.volumes.push({ containerVolume: templateVolume })
+      })
+  
+      this.template.files.forEach(templateFile => {
+        delete templateFile.hostPath
+        delete templateFile.fileContent
+  
+        templateToServiceFormat.files.push({ ...templateFile })
+      })
+  
+      this.template.ports.forEach(templatePort => {
+        templateToServiceFormat.ports.push({ containerPort: templatePort })
+      })
+  
+      this.template.envs.forEach(templateEnv => {
+        delete templateEnv.defaultValue
+  
+        templateToServiceFormat.envs.push(templateEnv)
+      })
+  
+      this.template.constraints.forEach(templateConstraint => {
+        delete templateConstraint.defaultValue
+  
+        templateToServiceFormat.constraints.push(templateConstraint)
+      })
+  
+      return templateToServiceFormat
     }
+  
+
+    /**
+     * A getter that returns the service object with irrelevant fields removed.
+     * @returns {object} An object containing only the relevant fields from the service object.
+    */
 
     get serviceWithoutUselessInfo() {
-        const serviceWithoutUselessInfo = {...this.service}
-
-        Object.keys(serviceWithoutUselessInfo).forEach((property) => {
-            if (!this.fieldsToCompare.includes(property)) {
-                delete serviceWithoutUselessInfo[property]
-            } else {
-                switch (property) {
-                    case "volumes":
-                        serviceWithoutUselessInfo["volumes"].forEach(volume => delete volume["hostVolume"])
-                        break;
-                    case "files":
-                        serviceWithoutUselessInfo["files"].forEach(file => {
-                            delete file["hostPath"]
-                            delete file["fileContent"]
-                        })
-                        break;
-                    case "ports":
-                        serviceWithoutUselessInfo["ports"].forEach(port => {
-                            delete port["hostPort"]
-                        })
-                        break;
-                    case "envs":
-                        serviceWithoutUselessInfo["envs"].forEach(env => {
-                            delete env["value"]
-                        })
-                        break;
-
-                    case "constraints":
-                        serviceWithoutUselessInfo["constraints"].forEach(constraint => {
-                            delete constraint["value"]
-                        })
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-        })
-
-        return serviceWithoutUselessInfo
+      const serviceWithoutUselessInfo = { ...this.service }
+  
+      Object.keys(serviceWithoutUselessInfo).forEach((property) => {
+        if (!this.fieldsToCompare.includes(property)) {
+          delete serviceWithoutUselessInfo[property]
+        } else {
+          switch (property) {
+            case "volumes":
+              serviceWithoutUselessInfo["volumes"].forEach(volume => delete volume["hostVolume"])
+              break
+            case "files":
+              serviceWithoutUselessInfo["files"].forEach(file => {
+                delete file["hostPath"]
+                delete file["fileContent"]
+              })
+              break
+            case "ports":
+              serviceWithoutUselessInfo["ports"].forEach(port => {
+                delete port["hostPort"]
+              })
+              break
+            case "envs":
+              serviceWithoutUselessInfo["envs"].forEach(env => {
+                delete env["value"]
+              })
+              break
+  
+            case "constraints":
+              serviceWithoutUselessInfo["constraints"].forEach(constraint => {
+                delete constraint["value"]
+              })
+              break
+  
+            default:
+              break
+          }
+        }
+      })
+  
+      return serviceWithoutUselessInfo
     }
-
+  
 
     /**
-     * Determines whether the `service` object is different from the `template` object only taking into account the properties listed in `fieldsToCompare` 
-     * @type {boolean}
+     * A getter that returns whether the service object matches the service template object in the relevant fields.
+     * @returns {boolean} `true` if the service object is different from the service template object in any of the
+     * relevant fields; `false` otherwise.
     */
+
     get serviceIsDifferent() {
+        const differences = []
         let isDifferent = false
-
+      
         this.fieldsToCompare.forEach(field => {
-            const templateField = JSON.stringify(this.templateToServiceFormat[field])
-            const serviceField = JSON.stringify(this.serviceWithoutUselessInfo[field])
-
-            console.log(`templateField: '${templateField}' |  serviceField '${serviceField}'`)
-
-            const templateSubset = templateField.startsWith('[') ? templateField.slice(1, -1) : templateField
-            const serviceSubset = serviceField.startsWith('[') ? serviceField.slice(1, -1) : serviceField
-
-            console.log(`templateSubset: '${templateSubset}' |  serviceSubset '${serviceSubset}'`)
-            console.log(`serviceIncludesTemplate: '${serviceSubset.includes(templateSubset)}'`)
-
-            if (!serviceSubset.includes(templateSubset)) isDifferent = true
+          const templateFieldObjects = this.templateToServiceFormat[field]
+          const serviceFieldObjects = this.serviceWithoutUselessInfo[field]
+      
+          const templateFieldCounts = {}
+          templateFieldObjects.forEach(object => {
+            const objectString = JSON.stringify(object)
+            templateFieldCounts[objectString] = (templateFieldCounts[objectString] || 0) + 1
+          })
+      
+          const serviceFieldCounts = {}
+          serviceFieldObjects.forEach(object => {
+            const objectString = JSON.stringify(object)
+            serviceFieldCounts[objectString] = (serviceFieldCounts[objectString] || 0) + 1
+          })
+      
+          const fieldIsDifferent = Object.keys(templateFieldCounts).some(key => {
+            const templateCount = templateFieldCounts[key]
+            const serviceCount = serviceFieldCounts[key]
+      
+            if (templateCount !== serviceCount) {
+              differences.push({ field, object: JSON.parse(key) })
+              return true
+            }
+      
+            return false
+          })
+      
+          if (fieldIsDifferent) {
+            isDifferent = true
+          }
         })
-
+      
+        console.log('Differences:', differences)
         return isDifferent
-    }
-
-
-}
+      }
+      
+      
+  }
+  
--- apps/frontend/src/helpers/serviceTemplateComparer.spec.js
+++ apps/frontend/src/helpers/serviceTemplateComparer.spec.js
@@ -1,57 +1,32 @@
+import { serviceTemplateObject, anotherServiceTemplateObject, serviceObject, anotherServiceObject, unorderedServiceTemplateObject, unorderedServiceObject } from './objects.js'
 import ServiceTemplateComparer from "./ServiceTemplateComparer.js"
-import { serviceTemplateObject, anotherServiceTemplateObject, serviceObject, anotherServiceObject } from './objects.js'
 
 describe("serviceTemplateComparer class test suite", () => {
 
+    // Relevant properties = serviceTemplateComparer.fieldsToCompare
+
     test('serviceTemplateComparer throws an error if it isnt instantiated with two objects to compare', () => {
-        try {
-            const serviceTemplateComparer = new ServiceTemplateComparer()
-        } catch (error) {
-            expect(error.message).toBe('The class must be instantiated with a template and a service')
-        }
+        expect(() => new ServiceTemplateComparer()).toThrow('The class must be instantiated with a template and a service')
     })
 
     test('map service template to service object with templateToServiceFormat method', () => {
-        const serviceTemplateComparer = new ServiceTemplateComparer(serviceTemplateObject, serviceObject)
-
-        // console.info(`serviceTemplateComparer.templateToServiceFormat: '${JSON.stringify(serviceTemplateComparer.templateToServiceFormat, null, 2)}'`)
-        // console.info(`serviceObject: '${JSON.stringify(serviceObject, null, 2)}'`)
-        // console.info(`serviceWithoutUselessInfo: '${JSON.stringify(serviceTemplateComparer.serviceWithoutUselessInfo, null, 2)}'`)
-        
-        expect(serviceTemplateComparer.templateToServiceFormat)
+        expect((new ServiceTemplateComparer(serviceTemplateObject, serviceObject)).templateToServiceFormat)
     })
 
-    test('serviceIsDifferent getter value is false if relevant properties (serviceTemplateComparer.fieldsToCompare) values match', () =>{
-        const serviceTemplateComparer = new ServiceTemplateComparer(serviceTemplateObject, serviceObject)
-
-        console.log(`serviceIsDifferent?: '${serviceTemplateComparer.serviceIsDifferent}'`)
-
-        expect(serviceTemplateComparer.serviceIsDifferent).toBe(false)
+    test('serviceIsDifferent getter value is true if relevant properties values do not match', () => {
+        expect((new ServiceTemplateComparer(anotherServiceTemplateObject, anotherServiceObject)).serviceIsDifferent).toBe(true)
+        expect((new ServiceTemplateComparer(anotherServiceTemplateObject, serviceObject)).serviceIsDifferent).toBe(true)
+        expect((new ServiceTemplateComparer(serviceTemplateObject, anotherServiceObject)).serviceIsDifferent).toBe(true)
     })
 
-    test('serviceIsDifferent getter value is true if relevant properties (serviceTemplateComparer.fieldsToCompare) values do not match', () =>{
-        const serviceTemplateComparer = new ServiceTemplateComparer(anotherServiceTemplateObject, serviceObject)
-
-        console.log(`serviceIsDifferent?: '${serviceTemplateComparer.serviceIsDifferent}'`)
-
-        expect(serviceTemplateComparer.serviceIsDifferent).toBe(true)
+    test('serviceIsDifferent getter value is false if relevant properties values match', () => {
+        expect((new ServiceTemplateComparer(serviceTemplateObject, serviceObject)).serviceIsDifferent).toBe(false)
     })
 
-    test('serviceIsDifferent getter value is true if relevant properties (serviceTemplateComparer.fieldsToCompare) values do not match (testing with different serviceObject)', () =>{
-        const serviceTemplateComparer = new ServiceTemplateComparer(serviceTemplateObject, anotherServiceObject)
-
-        console.log(`serviceIsDifferent?: '${serviceTemplateComparer.serviceIsDifferent}'`)
-        
-        expect(serviceTemplateComparer.serviceIsDifferent).toBe(true)
+    test('serviceIsDifferent getter value is false if relevant properties values match but they are in different order', () => {
+        expect((new ServiceTemplateComparer(unorderedServiceTemplateObject, unorderedServiceObject)).serviceIsDifferent).toBe(false)
     })
 
-    test('serviceIsDifferent getter value is true if relevant properties (serviceTemplateComparer.fieldsToCompare) values do not match (testing with different serviceObject)', () =>{
-        const serviceTemplateComparer = new ServiceTemplateComparer(anotherServiceTemplateObject, anotherServiceObject)
-
-        console.log(`serviceIsDifferent?: '${serviceTemplateComparer.serviceIsDifferent}'`)
-        
-        expect(serviceTemplateComparer.serviceIsDifferent).toBe(true)
-    })
 })
 
 
