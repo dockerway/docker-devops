a9e968cd606f0bc3dca92372d3678ca029c4bd1a
ramiro.sandoval <ramiro_sando@outlook.com>
2023-05-24 18:39:39 -0300

    changes on environmentServiceList

--- apps/frontend/src/modules/devops/pages/crud/EnvironmentServicePage/EnvironmentServiceList/index.js
+++ apps/frontend/src/modules/devops/pages/crud/EnvironmentServicePage/EnvironmentServiceList/index.js
@@ -1,3 +1,3 @@
-import EnvironmentServiceList from './EnvironmentServiceList'
+import EnvironmentServiceList from './EnvironmentServiceList.vue'
 export {EnvironmentServiceList}
 export default EnvironmentServiceList
