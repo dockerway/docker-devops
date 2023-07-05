a845fec78c6df5304afa2c168ed4731c0cccc981
ramiro.sandoval <ramiro_sando@outlook.com>
2023-07-05 10:53:33 -0300

    (feat): now environment combobox's items are ordered

--- apps/frontend/src/modules/devops/components/EnvironmentCombobox/EnvironmentCombobox.vue
+++ apps/frontend/src/modules/devops/components/EnvironmentCombobox/EnvironmentCombobox.vue
@@ -24,51 +24,67 @@
 
 <script>
 
-    import {InputErrorsByProps, RequiredRule} from '@dracul/common-frontend'
-    
-    
-    import EnvironmentProvider from "../../providers/EnvironmentProvider"
+import { InputErrorsByProps, RequiredRule } from '@dracul/common-frontend'
 
-    export default {
-        name: "EnvironmentCombobox",
-        mixins: [InputErrorsByProps, RequiredRule],
-        props:{
-            value: {type: [String, Array]},
-            multiple: {type:Boolean, default: false },
-            solo: {type:Boolean, default: false},
-            chips: {type:Boolean, default: false},
-            readonly: {type:Boolean, default:false},
-            disabled: {type:Boolean, default: false},
-            isRequired: {type:Boolean, default: false},
-            clearable: {type:Boolean, default: false},
-        },
-        data() {
-            return {
-                items: [],
-                loading: false
-            }
-        },
-        computed: {
-           item: {
-                get() { return this.value },
-                set(val) {this.$emit('input', val)}
-            }
-        },
-        async mounted() {
-         await this.fetch()
-        },
-        methods: {
-            validate(){
-              return this.$refs.form.validate()
-            },
-            async fetch(){
-                this.loading = true
-                this.items = (await EnvironmentProvider.fetchEnvironment()).data.fetchEnvironment
-                this.loading = false
-            }
-            
+
+import EnvironmentProvider from "../../providers/EnvironmentProvider"
+
+export default {
+    name: "EnvironmentCombobox",
+    mixins: [InputErrorsByProps, RequiredRule],
+    props: {
+        value: { type: [String, Array] },
+        multiple: { type: Boolean, default: false },
+        solo: { type: Boolean, default: false },
+        chips: { type: Boolean, default: false },
+        readonly: { type: Boolean, default: false },
+        disabled: { type: Boolean, default: false },
+        isRequired: { type: Boolean, default: false },
+        clearable: { type: Boolean, default: false },
+    },
+    data() {
+        return {
+            items: [],
+            loading: false
         }
+    },
+    computed: {
+        item: {
+            get() { return this.value },
+            set(val) { this.$emit('input', val) }
+        }
+    },
+    async mounted() {
+        await this.fetch()
+    },
+    methods: {
+        validate() {
+            return this.$refs.form.validate()
+        },
+        async fetch() {
+            this.loading = true
+            this.items = (await EnvironmentProvider.fetchEnvironment()).data.fetchEnvironment
+            this.sortItemsByName()
+            this.loading = false
+        },
+
+        sortItemsByName() {
+            this.items.sort((itemA, itemB) => {
+                const nameA = itemA.name.toLowerCase()
+                const nameB = itemB.name.toLowerCase()
+
+                if (nameA < nameB) {
+                    return -1
+                } else if (nameA > nameB) {
+                    return 1
+                } else {
+                    return 0
+                }
+            })
+        }
+
     }
+}
 </script>
 
 <style scoped>
